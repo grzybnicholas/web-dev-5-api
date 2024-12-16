3c23ff5889c508e52074a88285df3261c019cd99
grzybnicholas <nicholas.grzyb24@myhunter.cuny.edu>
2024-12-15 19:53:19 -0500

    heroku fix

--- app.js
+++ app.js
@@ -61,7 +61,8 @@ app.delete('/api/tasks/:id', async (req, res) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
